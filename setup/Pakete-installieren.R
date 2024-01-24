4b2d753766854451b3e85263b3d755743c8f8749
luebby <karsten@statistix.org>
2024-01-24 13:07:35 +0100

    weitere Pakete

--- setup/Pakete-installieren.R
+++ setup/Pakete-installieren.R
@@ -1,6 +1,7 @@
 # Installiert die nötigen R Pakete
 install.packages(c("mosaic", "lsr", "GGally", "corrplot", 
                    "googlesheets4", "readxl", "here",
+                   "DT", "webshot2",
                    "tinytex", "rmarkdown", "knitr",
                    "markdown","shiny", "gridExtra"))
 
